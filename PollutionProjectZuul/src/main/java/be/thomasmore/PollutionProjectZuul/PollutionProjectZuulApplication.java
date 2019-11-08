70e2ca5f499b69f745a6dfb52c9c1f748e71a5b8
Emmy Bruynseels <emmy.bruynseels@hotmail.com>
2019-11-08 11:10:01 +0100

    Zuul server

---
+++ PollutionProjectZuul/src/main/java/be/thomasmore/PollutionProjectZuul/PollutionProjectZuulApplication.java
@@ -0,0 +1,17 @@
+package be.thomasmore.PollutionProjectZuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZuulProxy
+public class PollutionProjectZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PollutionProjectZuulApplication.class, args);
+	}
+
+}
