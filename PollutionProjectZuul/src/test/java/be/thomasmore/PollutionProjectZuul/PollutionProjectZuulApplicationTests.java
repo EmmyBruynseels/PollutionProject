2c4de6088c325b671beb16e819c17f6906cdc25e
Emmy Bruynseels <emmy.bruynseels@hotmail.com>
2019-11-08 11:01:56 +0100

    zuul Server

---
+++ PollutionProjectZuul/src/test/java/be/thomasmore/PollutionProjectZuul/PollutionProjectZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package be.thomasmore.PollutionProjectZuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PollutionProjectZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
